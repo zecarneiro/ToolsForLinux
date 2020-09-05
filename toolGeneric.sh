29bdc08bd29e913d3e36e27b852984146a4ba83d
zecarneiro <zecarneiro44@gmail.com>
2020-09-05 22:40:29 +0100

    Update appsOperations.sh and toolGeneric.sh

--- toolGeneric.sh
+++ toolGeneric.sh
@@ -85,38 +85,7 @@ function existFile(){
 	echo $response
 }
 
-# Kill app with pid
-function killPID(){
-	local pid="$1"
-	local -i existPID
-	local -a subprocessPID=()
 
-	if [ ! -z "$pid" ]; then
-		# Check if pid exist
-		existPID=$(ps ax | grep $pid | grep -v grep | grep -c .)
-
-		# Kill All PIDs
-		if [ $existPID -gt 0 ]; then
-		    subprocessPID=( "$(pgrep -P $pid)" )
-
-		    # Kill PID Pai
-		    kill $pid 2> /dev/null
-
-		    for PID in "${subprocessPID[@]}"; do
-		        if [ ! -z "$PID" ]; then
-		            # Check if pid exist
-                    existPID=$(ps ax | grep $PID | grep -v grep | grep -c .)
-
-                    # add pid
-                    if [ $existPID -gt 0 ]; then
-                        # Kill subprocess PID
-		                kill $PID 2> /dev/null
-                    fi
-		        fi
-            done
-		fi
-	fi
-}
 
 # Delete File
 function removeFile(){
@@ -144,80 +113,6 @@ function removeDir(){
     fi
 }
 
-# Create Boot Desktop files
-function createBootDesktop(){
-    local appName="$1"
-	local cmdToExec="$2"
-    local useTerminal="$3"
-	local icon="$4"
-    local extraLines="$5"
-    local autoStartApp="$home/.config/autostart"
-    local desktopFullPath="$autoStartApp/$appName.desktop"
-    local desktopFiletext
-
-    # Create path
-    mkdir -p "$autoStartApp"
-
-    # Auto start app
-    desktopFiletext="[Desktop Entry]"
-    desktopFiletext="$desktopFiletext\nType=Application"
-    desktopFiletext="$desktopFiletext\nExec=$cmdToExec"
-    desktopFiletext="$desktopFiletext\nIcon=$icon"
-    desktopFiletext="$desktopFiletext\nHidden=false"
-    desktopFiletext="$desktopFiletext\nNoDisplay=false"
-
-    # Define if use terminal or not
-    if [ $useTerminal -eq 1 ]; then
-        desktopFiletext="$desktopFiletext\nTerminal=true"
-    else
-        desktopFiletext="$desktopFiletext\nTerminal=false"
-    fi
-
-    desktopFiletext="$desktopFiletext\nX-GNOME-Autostart-enabled=true"
-    desktopFiletext="$desktopFiletext\nName[pt]=$appName"
-    desktopFiletext="$desktopFiletext\nName=$appName"
-    desktopFiletext="$desktopFiletext\nComment[pt]="
-    desktopFiletext="$desktopFiletext\nComment=\n"
-    desktopFiletext="$desktopFiletext\n$extraLines"
-    printf "$desktopFiletext" | tee "$desktopFullPath" > /dev/null
-    chmod +x "$desktopFullPath"
-}
-
-# Create Normal Desktop files
-function createNormalDesktop(){
-	local appName="$1"
-	local cmdToExec="$2"
-    local useTerminal="$3"
-	local icon="$4"
-    local extraLines="$5"
-	local normalApp="$home/.local/share/applications"
-	local desktopFullPath="$normalApp/$appName.desktop"
-    local desktopFiletext
-
-    # Create path
-    mkdir -p "$normalApp"
-
-    # Normal App
-    desktopFiletext="[Desktop Entry]"
-    desktopFiletext="$desktopFiletext\nType=Application"
-    desktopFiletext="$desktopFiletext\nExec=$cmdToExec"
-    desktopFiletext="$desktopFiletext\nIcon=$icon"
-
-    # Define if use terminal or not
-    if [ $useTerminal -eq 1 ]; then
-        desktopFiletext="$desktopFiletext\nTerminal=true"
-    else
-        desktopFiletext="$desktopFiletext\nTerminal=false"
-    fi
-
-    desktopFiletext="$desktopFiletext\nName[pt]=$appName"
-    desktopFiletext="$desktopFiletext\nName=$appName"
-    desktopFiletext="$desktopFiletext\nGenericName=$appName\n"
-    desktopFiletext="$desktopFiletext\n$extraLines"
-    printf "$desktopFiletext" | tee "$desktopFullPath" > /dev/null
-    chmod +x "$desktopFullPath"
-}
-
 # Execute command and return output
 function execCommandGetOutput(){
     local command="$1"
@@ -235,49 +130,6 @@ function chownCmd(){
 
 	# Execute command
 	eval "sudo chown -R $user:$group $pathFileOrDir"
-}
-
-# Install DEB Files
-function installDebFiles(){
-	local -a apps=($1)
-
-	# Install
-    for APP in "${apps[@]}"; do
-        echo "Install $APP..."
-        sudo gdebi -n "$APP"
-        printEmptyLines 2
-    done
-}
-
-# Install RPM Files
-function installRpmFiles(){
-	local -a apps=($1)
-
-	# Install
-    for APP in "${apps[@]}"; do
-        echo "Install $APP..."
-        sudo alien -i "$APP"
-        printEmptyLines 2
-    done
-}
-
-# Install Gnome Shell Extension
-function installGnomeShellExtension(){
-    local zipFile="$1"
-    local uuid="$(unzip -c "$zipFile" metadata.json | grep uuid | cut -d \" -f4)"
-
-    if [ ! -z $uuid ]; then
-        local extensionsPath="$home/.local/share/gnome-shell/extensions/$uuid"
-
-        # Create extension path
-        mkdir -p "$extensionsPath"
-
-        # Install Extension
-        unzip -q "$zipFile" -d "$extensionsPath/"
-        gnome-shell-extension-tool -e "$uuid"
-    else
-        echo "ERROR: Failed on get UUID"
-    fi
 }
 
 # Install Linux Mint Applets
