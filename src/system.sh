f2dc746a3b0f6355211fcb285aeaf0c2da11bed9
zecarneiro <zecarneiro44@gmail.com>
2020-11-15 14:16:20 +0000

    Update system.sh

--- src/system.sh
+++ src/system.sh
@@ -637,7 +637,7 @@ function appFLATPAK() {
 
     case "$operation" in
         i)
-            cmd="flatpak install %APP%"
+            cmd="flatpak install --system --user %APP%"
             cmd="${cmd} || torsocks ${cmd}"
             errorAPP="INSTALL FAIL APP: "
         ;;
